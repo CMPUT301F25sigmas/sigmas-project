8d9b1bcdfa2cadd13d60ad733d38fc9f3d59a772
udaygarg13 <udaygarg2003@gmail.com>
2025-10-30 15:43:17 -0600

    Created basic backend structure

--- code/app/src/main/java/com/example/atlasevents/MainActivity.java
+++ code/app/src/main/java/com/example/atlasevents/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.atlasevents;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,18 +9,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.atlasevents.data.UserRepository;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
+    private UserRepository userRepo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
+        userRepo = new UserRepository();
+
+        Entrant newUser = new Entrant(
+                "Alice",
+                "alice@example1.com",
+                "mypassword",
+                "1234567890"
+        );
+
+        userRepo.addUser(newUser)
+                .addOnSuccessListener(aVoid ->
+                        Log.d("Firestore", "User added successfully"))
+                .addOnFailureListener(e ->
+                        Log.e("Firestore", "Failed to add user", e));
     }
+
 }

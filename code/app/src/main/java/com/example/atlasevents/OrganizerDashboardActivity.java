cbfcf97d86aa1364f27450e3c77a8f6178917222
naser-cpu <naissa1@ualberta.ca>
2025-11-05 19:40:17 -0700

    Fixed broken logout button for organizer

--- code/app/src/main/java/com/example/atlasevents/OrganizerDashboardActivity.java
+++ code/app/src/main/java/com/example/atlasevents/OrganizerDashboardActivity.java
@@ -21,13 +21,7 @@ public class OrganizerDashboardActivity extends OrganizerBase {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.organizer_dashboard_empty);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentLayout(R.layout.organizer_dashboard_empty);
         session = new Session(this);
 
         // Set up notification icon click listener

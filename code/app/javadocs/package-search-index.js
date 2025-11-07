92f6d25098fa7464c2c4979d7e553f59d2bfa3b8
udaygarg13 <udaygarg2003@gmail.com>
2025-11-06 23:14:46 -0700

    Moved javadocs to docs

--- code/app/javadocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.atlasevents"},{"l":"com.example.atlasevents.data"},{"l":"com.example.atlasevents.data.model"},{"l":"com.example.atlasevents.utils"}];updateSearchResults();

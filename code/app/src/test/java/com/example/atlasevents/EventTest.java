f22411d3ea07f3d3df05044ce185e5dc93c75070
udaygarg13 <udaygarg2003@gmail.com>
2025-11-06 20:36:40 -0700

    Moved tests

--- code/app/src/test/java/com/example/atlasevents/EventTest.java
+++ code/app/src/test/java/com/example/atlasevents/EventTest.java
@@ -3,7 +3,6 @@ package com.example.atlasevents;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.text.SimpleDateFormat;
